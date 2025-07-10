af04d53419e8affcc691fe3aef12e2427a790762
oklenum <71653767+oklenum@users.noreply.github.com>
2025-07-10 23:52:21 +0200

    chore: added splash screen and loading

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,11 +11,16 @@ import { AuthProvider } from "@/hooks/useAuth";
 
 export const metadata = {
   title: "UGE 30",
-  description: "Classement for UGE 30",
+  description: "Klassement for UGE 30",
   icons: {
-    icon: "/uge30gul-192.jpg",
-    apple: "/uge30gul-180.png",
-    shortcut: "/uge30gul-192.jpg",
+    icon: "/android/android-launchericon-192-192.png",
+    apple: "/ios/180.png",
+    shortcut: "/android/android-launchericon-192-192.png",
+  },
+  appleWebApp: {
+    capable: true,
+    title: "UGE 30",
+    statusBarStyle: "default",
   },
 };
 
@@ -39,6 +44,31 @@ export default async function RootLayout({
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1.0, user-scalable=no"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="/apple-splash/apple-splash-1170x2532.png"
+          media="(device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3)"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="/apple-splash/apple-splash-1284x2778.png"
+          media="(device-width: 428px) and (device-height: 926px) and (-webkit-device-pixel-ratio: 3)"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="/apple-splash/apple-splash-828x1792.png"
+          media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="/apple-splash/apple-splash-1125x2436.png"
+          media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="/apple-splash/apple-splash-1242x2688.png"
+          media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)"
         />
       </head>
       <body className="min-h-screen bg-gradient-to-br from-slate-900 via-blue-900 to-cyan-900">
