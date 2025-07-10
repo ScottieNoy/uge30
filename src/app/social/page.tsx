081c47a1135178fd21811254a6571d21e0db0b88
oklenum <71653767+oklenum@users.noreply.github.com>
2025-07-10 17:15:22 +0200

    protect social page

--- src/app/social/page.tsx
+++ src/app/social/page.tsx
@@ -1,7 +1,14 @@
 import { Suspense } from "react";
 import Social from "@/components/Social";
+import { createClient } from "@/lib/supabaseServer";
+import { redirect } from "next/navigation";
 
-export default function SocialPage() {
+export default async function SocialPage() {
+  const supabase = await createClient();
+  const { data, error } = await supabase.auth.getUser();
+  if (error || !data?.user) {
+    redirect("/");
+  }
   return (
     <Suspense
       fallback={<div className="text-white p-8">Loading social hub...</div>}
