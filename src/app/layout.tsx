f9c6a6ca10d35eb951dd8ab2acd54abdeaef75a9
ScottieNoy <oscar@stromsborg.dk>
2025-11-05 19:48:07 +0100

    under maintanence: change layout file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,60 +29,63 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <html lang="da">
-      <head>
-        <link rel="manifest" href="/manifest.json" />
-        <link rel="apple-touch-icon" href="/uge30gul-180.png" />
-        <meta name="theme-color" content="#000000" />
-        <meta name="mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta
-          name="apple-mobile-web-app-status-bar-style"
-          content="black-translucent"
-        />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0, user-scalable=no"
-        />
-        <link
-          rel="apple-touch-startup-image"
-          href="/apple-splash/apple-splash-1170x2532.png"
-          media="(device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3)"
-        />
-        <link
-          rel="apple-touch-startup-image"
-          href="/apple-splash/apple-splash-1284x2778.png"
-          media="(device-width: 428px) and (device-height: 926px) and (-webkit-device-pixel-ratio: 3)"
-        />
-        <link
-          rel="apple-touch-startup-image"
-          href="/apple-splash/apple-splash-828x1792.png"
-          media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)"
-        />
-        <link
-          rel="apple-touch-startup-image"
-          href="/apple-splash/apple-splash-1125x2436.png"
-          media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)"
-        />
-        <link
-          rel="apple-touch-startup-image"
-          href="/apple-splash/apple-splash-1242x2688.png"
-          media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)"
-        />
-      </head>
-      <body className="min-h-screen bg-gradient-to-br from-slate-900 via-blue-900 to-cyan-900">
-        <AuthProvider>
-          <PwaInit />
-          {/* <NotificationManager /> */}
-          <NotificationGate />
-          <Navbar />
-          <ClientRoot />
+  // return (
+  //   <html lang="da">
+  //     <head>
+  //       <link rel="manifest" href="/manifest.json" />
+  //       <link rel="apple-touch-icon" href="/uge30gul-180.png" />
+  //       <meta name="theme-color" content="#000000" />
+  //       <meta name="mobile-web-app-capable" content="yes" />
+  //       <meta name="apple-mobile-web-app-capable" content="yes" />
+  //       <meta
+  //         name="apple-mobile-web-app-status-bar-style"
+  //         content="black-translucent"
+  //       />
+  //       <meta
+  //         name="viewport"
+  //         content="width=device-width, initial-scale=1.0, user-scalable=no"
+  //       />
+  //       <link
+  //         rel="apple-touch-startup-image"
+  //         href="/apple-splash/apple-splash-1170x2532.png"
+  //         media="(device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3)"
+  //       />
+  //       <link
+  //         rel="apple-touch-startup-image"
+  //         href="/apple-splash/apple-splash-1284x2778.png"
+  //         media="(device-width: 428px) and (device-height: 926px) and (-webkit-device-pixel-ratio: 3)"
+  //       />
+  //       <link
+  //         rel="apple-touch-startup-image"
+  //         href="/apple-splash/apple-splash-828x1792.png"
+  //         media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)"
+  //       />
+  //       <link
+  //         rel="apple-touch-startup-image"
+  //         href="/apple-splash/apple-splash-1125x2436.png"
+  //         media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)"
+  //       />
+  //       <link
+  //         rel="apple-touch-startup-image"
+  //         href="/apple-splash/apple-splash-1242x2688.png"
+  //         media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)"
+  //       />
+  //     </head>
+  //     <body className="min-h-screen bg-gradient-to-br from-slate-900 via-blue-900 to-cyan-900">
+  //       <AuthProvider>
+  //         <PwaInit />
+  //         {/* <NotificationManager /> */}
+  //         <NotificationGate />
+  //         <Navbar />
+  //         <ClientRoot />
 
-          <main>{children}</main>
-          <Toaster />
-        </AuthProvider>
-      </body>
-    </html>
+  //         <main>{children}</main>
+  //         <Toaster />
+  //       </AuthProvider>
+  //     </body>
+  //   </html>
+  // );
+  return (
+    <div>App disabled for maintenance</div>
   );
 }
